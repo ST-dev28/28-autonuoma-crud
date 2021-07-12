015f065aae28ea0c4135d4120a86ae39fbf1054a
SandraT <sndrbltr@gmail.com>
2021-07-12 17:15:01 +0000

    CityBird crud done

--- CarRental.js
+++ CarRental.js
@@ -1,12 +1,67 @@
 class CarRental {
-    constructor() {
-        this.list =[];
+    constructor(name) {
+        this.list = [];
+        this.companyName = name;
     }
+
     intro() {
-        console.log('Hi, we are CityBird and we have 0 cars available.');
+        let count = 0;
+        for (let i = 0; i < this.list.length; i++) {
+            const car = this.list[i];
+            if (car.good === true) {
+                count++;
+            }
+        }
+        console.log(`Hi, we are ${this.companyName} and we have ${count} cars available.`);
     }
-    addCar(car) {
-        this.list.push(car);
+
+    addCar(model, color, price) {
+        this.list.push({ model, color, price, good: true });
+        //arba
+        //let carInfo = {model, color, price, good}
+        // this.list.push(carInfo);
+
+        console.log('----------------');
+    }
+
+    carPark() {
+        let carNumber = 0;
+        for (let i = 0; i < this.list.length; i++) {
+            const car = this.list[i];
+            carNumber++;
+            if (car.good === true) {
+                console.log(`${carNumber}. ${car.model} (${car.color}) is for ${car.price}EUR/day`)
+            }
+        }
+        console.log('----------------');
+    }
+    updateCarPrice(index, newPrice) {
+        this.list[index].price = newPrice;
+
+        console.log('----------------');
+    }
+    carAccident(index) {
+        this.list[index].good = false;
+
+        console.log('------------------');
+    }
+
+    carRepair(index) {
+        this.list[index].good = true;
+
+        console.log('------------------');
+    }
+    removeCar(index) {
+        const updatedList = [];
+
+        for (let i = 0; i < this.list.length; i++) {
+            if (i !== index) {
+                updatedList.push(this.list[i]);
+            }
+        }
+        this.list = updatedList; //perrasau LIST reiksme (overwrite)
+
+        console.log('------------------');
     }
 }
 
--- index.js
+++ index.js
@@ -11,11 +11,11 @@ cityBird.addCar('Toyota Yaris', 'black', 25);
 cityBird.addCar('Volvo S40', 'red', 20);
 cityBird.addCar('Tesla 3', 'white', 40);
 
-//console.log(cityBird);
+console.log(cityBird);
 
 cityBird.intro();
 // Hi, we are CityBird and we have 3 cars available.
-/*
+
 cityBird.carPark();
 // 1. Toyota Yaris (black) is for 25EUR/day.
 // 2. Volvo S40 (red) is for 20EUR/day.
@@ -57,4 +57,3 @@ cityBird.intro();
 cityBird.carPark();
 // 1. Volvo S40 (red) is for 20EUR/day.
 // 2. Tesla 3 (white) is for 35EUR/day.
-*/
